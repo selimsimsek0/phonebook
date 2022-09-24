9bd4d4315d34ae7ded5545fb150baa66fd2d04ba
Selim Can Şimşek <selimcan0658@gmail.com>
2022-09-24 17:30:55 +0300

    added service and managers

---
+++ src/PhoneBook/PhoneBook.Business/Abstract/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using PhoneBook.Entity.Entity;
+using System;
+using System.Collections.Generic;
+
+namespace PhoneBook.Business.Abstract
+{
+    public interface IPersonService
+    {
+        Person GetPersonById(Guid id);
+        Person GetPersonByIdWithContactInfos(Guid id);
+        List<Person> GetAllPerson();
+        List<Person> GetAllPersonWithContactInfos();
+        bool DeletePerson(Guid id);
+        bool DeletePerson(Person person);
+        bool AddPerson(Person person);
+        bool UpdatePerson(Person person);
+    }
+}
